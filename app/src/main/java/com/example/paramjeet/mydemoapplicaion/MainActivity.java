f8066b35187299580d84ac3623d32c35d0b39ea7
pegasussystem <33860191+pegasussystem@users.noreply.github.com>
2018-01-15 11:47:38 +0530

    Listener added in java

--- app/src/main/java/com/example/paramjeet/mydemoapplicaion/MainActivity.java
+++ app/src/main/java/com/example/paramjeet/mydemoapplicaion/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.paramjeet.mydemoapplicaion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText et= (EditText) findViewById(R.id.et1);
+        Button btn= (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, et.getText().toString(), Toast.LENGTH_SHORT).show();
+                et.setText("");
+            }
+        });
     }
 }
